c6c098b481cf91f2ed90ca124a1492c87008f6d5
Adam Braly <35744310+adambraly@users.noreply.github.com>
2024-01-09 15:10:35 -0500

    Last major object update
    Added a player class to store player display information

---
+++ Objects/Player.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Talos.Enumerations;
+using Talos.Structs;
+
+namespace Talos.Objects
+{
+    internal sealed class Player : Creature
+    {
+        internal bool _isHidden;
+
+        internal ushort HeadSprite { get; set; }
+        internal ushort ArmorSprite { get; set; }
+        internal ushort ArmorSprite2 { get; set; }
+        internal ushort WeaponSprite { get; set; }
+        internal ushort AccessorySprite1 { get; set; }
+        internal ushort AccessorySprite2 { get; set; }
+        internal ushort AccessorySprite3 { get; set; }
+        internal ushort OvercoatSprite { get; set; }
+        internal byte BodySprite { get; set; }
+        internal byte BootsSprite { get; set; }
+        internal byte ShieldSprite { get; set; }
+        internal byte HeadColor { get; set; }
+        internal byte BootColor { get; set; }
+        internal byte AccessoryColor1 { get; set; }
+        internal byte AccessoryColor2 { get; set; }
+        internal byte AccessoryColor3 { get; set; }
+        internal byte LanternSize { get; set; }
+        internal byte RestPosition { get; set; }
+        internal byte OvercoatColor { get; set; }
+        internal byte BodyColor { get; set; }
+        internal byte FaceSprite { get; set; }
+        internal byte NameTagStyle { get; set; }
+        internal string GroupName { get; set; }
+        internal bool NeedsHeal { get; set; }
+        internal new string Name { get { return base.Name; } set { base.Name = value; } }
+        internal DateTime LastArmachd { get; set; }
+        internal bool HasArmachd
+        {
+            get
+            {
+                if (!base.SpellAnimationHistory.ContainsKey(20))
+                    return false;
+                if (!(DateTime.UtcNow.Subtract(LastArmachd).TotalMinutes < 30.0))
+                    return DateTime.UtcNow.Subtract(base.SpellAnimationHistory[20]).TotalMinutes < 2.5;
+                return true;
+            }
+        }
+        internal bool IsSkulled
+        {
+            get
+            {
+                if (!base.SpellAnimationHistory.ContainsKey(24))
+                    return false;
+                return DateTime.UtcNow.Subtract(base.SpellAnimationHistory[24]).TotalSeconds < 2.0;
+            }
+        }
+
+        internal Player(int id, string name, Location location, Direction direction)
+            : base(id, name, 0, (byte)CreatureType.Aisling, location, direction)
+        {
+            Type = CreatureType.Aisling;
+            LastArmachd = DateTime.MinValue;
+        }
+
+        internal void NakedPlayer()
+        {
+            ArmorSprite = 0;
+            ArmorSprite2 = 0;
+            WeaponSprite = 0;
+            AccessorySprite1 = 0;
+            AccessorySprite2 = 0;
+            AccessorySprite3 = 0;
+            OvercoatSprite = 0;
+            BodySprite = 0;
+            BootsSprite = 0;
+            ShieldSprite = 0;
+            HeadColor = 0;
+            BootColor = 0;
+            AccessoryColor1 = 0;
+            AccessoryColor2 = 0;
+            AccessoryColor3 = 0;
+            LanternSize = 0;
+            RestPosition = 0;
+            OvercoatColor = 0;
+        }
+    }
+}
